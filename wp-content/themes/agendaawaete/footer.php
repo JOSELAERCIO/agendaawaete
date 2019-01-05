abf43eeadea3c6b92cb8e02a3abc625f0e09fbfe
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2019-01-05 21:16:16 +0000

    Create footer

--- wp-content/themes/agendaawaete/footer.php
+++ wp-content/themes/agendaawaete/footer.php
@@ -1,5 +1,15 @@
-<footer>
-
+<footer class="footer">
+    <div class="footer-social-media">
+        <div class="footer-instagram">
+            <p class="text-footer">Instagram Agenda Awaete</p>
+        </div>
+        <div class="footer-social-icons">
+            <p class="text-footer">Conecte-se nas redes</p>
+        </div>
+    </div>
+    <div class="to-top">
+        <a href="#" title="Para o topo" class="text-footer">acima</a>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
