e752de728f2b64eb80db1863393df6e81bea5ff6
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2019-01-30 20:32:51 +0000

    create post-meta-container and entry-author

--- wp-content/themes/agendaawaete/parts/content-single.php
+++ wp-content/themes/agendaawaete/parts/content-single.php
@@ -6,3 +6,19 @@
     </header>
     <?php the_content(); ?>
 </article>
+
+<div class="post-meta-container">
+    <div class="entry-author">
+        <figure class="avatar-container">
+            <?php 
+                echo get_avatar( get_the_author_meta( 'ID' ), 32 ); 
+            ?>
+        </figure>
+        <h2 class="author-title">Publicado por <?php the_author(); ?></h2>
+        <div class="author-bio">
+            <a class="author-link" target="_blank" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
+                Ver todos os posts de <?php the_author(); ?>
+            </a>
+        </div>
+    </div>
+</div>
