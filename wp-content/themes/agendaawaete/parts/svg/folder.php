7dd96959266a3a48805e8273da574c79434da21a
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2019-01-30 16:06:20 +0000

    commit 30/01/2019

---
+++ wp-content/themes/agendaawaete/parts/svg/folder.php
@@ -0,0 +1,5 @@
+<svg class="icon -folder" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 60 60" style="enable-background:new 0 0 60 60;" xml:space="preserve">
+	<polygon points="3.7,43.2 3.7,5.5 23.1,4.9 27.9,11.5 52.6,11.9 52.6,19.1 13,19.1 "/>
+	<polygon points="4.4,46 14.5,21.6 55.6,21.3 45.4,46.2 "/>
+</svg>
