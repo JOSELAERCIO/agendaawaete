77283472f390d775df65662429515ea8786b8aae
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2019-02-02 16:20:04 +0000

    Change input author, email in comment.php

--- wp-content/themes/agendaawaete/comments.php
+++ wp-content/themes/agendaawaete/comments.php
@@ -81,6 +81,20 @@ if ( post_password_required() ) {
                 'comment_field' =>  '<p class="comment-form-comment">
                  <textarea placeholder="Digite seu comentário aqui..." id="comment" name="comment" cols="45" rows="8" aria-required="true">' .
                 '</textarea></p>',
+				$fields =  array(
+
+					'author' =>
+					  '<p class="comment-form-author">
+						<input id="author" name="author" placeholder="Digite seu nome (obrigatório)" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+					  '" size="30"' . $aria_req . ' /></p>',
+				  
+					'email' =>
+					  '<p class="comment-form-email">
+					  <input id="email" placeholder="Digite seu email (obrigatório)" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+					  '" size="30"' . $aria_req . ' /></p>',
+				  
+				),
+				'fields' => apply_filters( 'comment_form_default_fields', $fields )
 			)
 		);
 		?>
